2f6269745d6b6c09af455af9bcf63bf7e769ebeb
Kurteezy <100117236+Kurteezy@users.noreply.github.com>
2022-04-03 23:18:32 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,10 +5,11 @@
     </head>
   <body>
     <h1>Welcome to My Website! I am Kurt Johnzel L. Blasurca</h1>
-    <ul>
-      <li><a href="">HOME</a></li> 
-      <li><a href="LT2/index.html">Ralphcutie</a></li>
-      <li><a href="index3.html">LT3</a></li>
+    <ul> 
+      <li><a href="LT2/index.html">Learning Task 2</a></li>
+      <li><a href="index3.html">Learning Task 3</a></li>
+      <li><a href="index3.html">CLEM1T</a></li>
+      <li><a href="index3.html">Learning Task 4</a></li>
     </ul>
   </body>
 </html>
