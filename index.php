f90140ad8f3b567386bbd921679d2faf30bfad7e
Kurteezy <100117236+Kurteezy@users.noreply.github.com>
2022-04-03 22:43:36 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>My Home Page</title>
+    </head>
+  <body>
+    <h1>Welcome to My Website! I am Kurt Johnzel L. Blasurca</h1>
+    <ul>
+      <li><a href="index.php">HOME</a></li> 
+      <li><a href="index.html">Learning Task</a></li>
+      <li><a href="index3.html">LT3</a></li>
+    </ul>
+  </body>
+</html>
